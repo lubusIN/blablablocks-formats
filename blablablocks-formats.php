bf541d789e30b669fe33bf5d528cf8f4af2cedc9
punitverma123 <punitv342@gmail.com>
2025-09-24 11:31:42 +0530

    chore: bump version to 1.1.1

--- blablablocks-formats.php
+++ blablablocks-formats.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:			BlaBlaBlocks Formats
  * Description:			Rich text formats from BlaBlaBlocks.
- * Version:				1.1.0
+ * Version:				1.1.1
  * Requires at least:	6.6
  * Requires PHP:		7.4
  * Author:				Lubus
