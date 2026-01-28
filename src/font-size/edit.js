51756af670b63433016f3771904fcade125921bf
Ajit Bohra <ajit@lubus.in>
2026-01-28 13:46:15 +0530

    feat: add fontsize format (#80)
    * feat: add fontsize format
    
    * fix: Ensure font size attributes are correctly applied by using the replace method and clearing conflicting class
    
    * fix: reset custom fontsize
    
    * update toolbar and popuover ui
    
    * chore: core refactor and UI polish
    
    * chore: code refactor

---
+++ src/font-size/edit.js
@@ -0,0 +1,55 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+import { RichTextToolbarButton } from '@wordpress/block-editor';
+import { textHorizontal } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import InlineUI from './inline-ui';
+
+/**
+ * Edit component for the FontSize format in the block editor.
+ *
+ * @param {Object}   props                  - The component properties.
+ * @param {Object}   props.value            - The current value of the rich text.
+ * @param {Function} props.onChange         - Function to update the rich text value.
+ * @param {boolean}  props.isActive         - Indicates if the format is currently active.
+ * @param {Object}   props.contentRef       - Reference to the editable content element.
+ * @param {Object}   props.activeAttributes - The currently active attributes.
+ * @return {JSX.Element} - The rendered FontSize format.
+ */
+export function Edit({
+	value,
+	onChange,
+	isActive,
+	contentRef,
+	activeAttributes,
+}) {
+	const [isSettingOpen, setIsSettingOpen] = useState(false);
+
+	return (
+		<>
+			<RichTextToolbarButton
+				icon={textHorizontal}
+				title={__('Font Size', 'blablablocks-formats')}
+				onClick={() => setIsSettingOpen(true)}
+				isActive={isActive}
+			/>
+
+			{isSettingOpen && (
+				<InlineUI
+					value={value}
+					onChange={onChange}
+					onClose={() => setIsSettingOpen(false)}
+					activeAttributes={activeAttributes}
+					contentRef={contentRef.current}
+					isActive={isActive}
+				/>
+			)}
+		</>
+	);
+}
